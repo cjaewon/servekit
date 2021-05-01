28009b97acf45c71c87830dc5399ccb4c18b7037
cjaewon <jwn4492@outlook.com>
2021-05-01 19:25:32 +0900

    Release v2.0.0

--- docker/deploy-image.sh
+++ docker/deploy-image.sh
@@ -1,2 +1,2 @@
-docker build -t cjaewon/servekit:1.0.0 -f ./docker/Dockerfile .
-docker push cjaewon/servekit:1.0.0
+docker build -t cjaewon/servekit:2.0.0 -f ./docker/Dockerfile .
+docker push cjaewon/servekit:2.0.0
